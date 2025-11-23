bd1b297a2d947fd3e2a48ab2f0df6abbc497083f
SagnikPratihar <sagnikpratihar02@gmail.com>
2025-11-23 12:31:45 +0530

    Setting up TS server for getstream

---
+++ chatbackend/src/index.ts
@@ -0,0 +1,22 @@
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
+import { apikey } from "./serverClient";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cors({ origin: "*" }));
+
+app.get("/", (req, res) => {
+  res.json;
+  {
+    message: "Server is running";
+    apikey: apikey;
+  }
+});
+
+const PORT = process.env.PORT || 30000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
